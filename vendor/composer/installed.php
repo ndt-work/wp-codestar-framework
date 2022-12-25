6a5cdb9b97087312698d0ab9968ccc2ea2cdf28c
tuend.work <tuend.work@gmail.com>
2022-12-26 02:48:53 +0700

    cm

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tuend-work/wp-codestar-framework',
+        'pretty_version' => '1.0.3',
+        'version' => '1.0.3.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tuend-work/wp-codestar-framework' => array(
+            'pretty_version' => '1.0.3',
+            'version' => '1.0.3.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
